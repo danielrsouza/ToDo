ae83749ba73da962ba7885472cf6ca79d6de609e
Daniel Souza <daniel.ricardo@rede.ulbra.br>
2019-11-01 20:01:52 -0300

    Todo Pronto

--- src/domain/Form.js
+++ src/domain/Form.js
@@ -4,9 +4,10 @@ import {
     Button,
     TextInput,
     StyleSheet,
-    style
+    FlatList,
+    TouchableOpacity,
+    Text
 } from 'react-native';
-import Todo from './Todo';
 import TodoClass from './TodoClass'
 
 class Form extends Component {
@@ -17,16 +18,30 @@ class Form extends Component {
         itens: []
     }
 
+    // Lida com salvar o estado do texto.
     handleSalvaTexto = (text) => {
         this.setState({text})
     }
     
+
+    // Lida com deletar uma tarefa.
+    handleDelete = (item) => {
+        const {itens} = this.state;
+        const listNova = itens.filter(listItem => item.id !== listItem.id )
+
+        this.setState({
+            itens: listNova
+        });
+    };
+    
+    // Lida com salvar uma tarefa.
     handleSalvaTarefa = () => {
         let itens;
 
         if (this.state.text != '') {
+
             itens = [...this.state.itens, new TodoClass(this.state.text)];
-        
+
             this.setState({
                 itens: itens
             });
@@ -39,17 +54,27 @@ class Form extends Component {
         this.setState({ itens, text: '', id: null})        
     }
 
-    handleDelete = (item) => {
-        const filteredList = this.state.itens.filter(itens => itens !== item);
-        this.setState({ itens: filteredList });
-    }
-
-
     render() {
+        const keyExtractor = (item) => item.id
 
         return(
             <View>
-                <Todo list={this.state.itens} handleDelete={handleDelete()} />
+                <View style={{flexDirection: 'row', margin: 5}}>
+                    <FlatList 
+                        data={this.state.itens}
+                        keyExtractor={keyExtractor}
+                        renderItem={item => (
+                            <TouchableOpacity style={styles.row}>
+                                <Text style={styles.texto}>{ item.item.text }</Text>
+                                <Button 
+                                    title="Remover" 
+                                    color="red"
+                                    onPress={() => this.handleDelete(item.item)} 
+                                />
+                            </TouchableOpacity>
+                        )}    
+                    />
+                </View>
 
                 <TextInput 
                     style={styles.input}
@@ -58,7 +83,7 @@ class Form extends Component {
                     onChangeText={this.handleSalvaTexto}
                 />
                 <Button 
-                    title="Adicionar uma Nova Tarefa"
+                    title="Adicionar nova tarefa"
                     color="red"
                     onPress={this.handleSalvaTarefa}
                 />
@@ -71,6 +96,22 @@ const styles = StyleSheet.create({
     input: {
         padding: 5,
         margin: 5,
+      },
+      row: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        backgroundColor: "#d9d9d9",
+        color: "black",
+        fontSize: 25,
+        margin: 10,
+        borderRadius: 10,
+        textAlign: 'center'
+      },
+      texto: {
+          textAlign: 'center',
+          fontSize: 20,
+          color: 'black',
+          padding: 10
       }
 })
 
